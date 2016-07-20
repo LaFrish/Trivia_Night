6822593b76e3c5490ac5ba7376a719a762d943a9
YOUR FULL NAME <farishta.haider@gmail.com>
2016-07-20 03:15:12 -0400

    Q&A container now hides when questions are finished

--- script.js
+++ script.js
@@ -43,9 +43,10 @@ function changeColor() {
 
 function trivia(){
   var categories;
-  var showCategories = document.getElementById("catagory");
+  var showCategories = document.getElementById("category");
   var showScore = document.getElementById("myScore");
   var question1 = $(".question");
+  // var click = document.getElementByClass("answers");
   // var question1 = document.querySelector(".question");
   // var optionA1 = $("#optionA");
   var optionA1 = document.getElementById("optionA");
@@ -56,7 +57,7 @@ function trivia(){
   var incorrectAnswer1 = document.querySelector(".incorrectAnswer");
   var options = document.querySelectorAll(".answerOptions");
   console.log(options);
-  //var arrayLength = question.length;
+  // var arrayLength = question.length;
   //var playerselection = []
   i = 0;
   score = 0
@@ -65,22 +66,21 @@ function trivia(){
 
   var selectCategory = function () {
     if (chosenCategory === categories[0]) {
-      catagoryName.innerHTML = "The Chosen Category Is Music";
+      categoryName.innerHTML = "The Chosen Category Is Music";
     } else if (chosenCategory === categories[1]) {
-      catagoryName.innerHTML = "The Chosen Category Is Technology";
+      categoryName.innerHTML = "The Chosen Category Is Technology";
     } else if (chosenCategory === categories[2]) {
-      catagoryName.innerHTML = "The Chosen Category Is Celebrities";
+      categoryName.innerHTML = "The Chosen Category Is Celebrities";
     }
   }
 
-  round = ["ROUND 1 </br> TV & Movies", "ROUND 2 </br> Saturday Night </br> Live", "ROUND 3 </br> Celebrity </br> Gossip"];
-  question = ["Welcome </br>to</br> Trivia Night!", "Which actor has a game based </br> off connecting any celebrity</br> to him in 6 degrees?", "For which role did </br> Leonardo DiCaprio </br> win an Oscar?", "Which movie has </br> won the </br> most Oscars?", "Which of these </br> actors has not </br> received an EGOT?", "Which movie </br> is not </br> based off a book?", "ROUND 2 </br> Saturday Night </br> Live", "Who has hosted </br> Saturday Night Live </br> the most times?", "Who has been </br> banned from <br> SNL?", "Who has not </br> been an anchor on </br> Weekend Update?", "Who was not </br> part of the <br> original SNL cast?", "Who was been a SNL </br> cast member the longest?", "ROUND 3 </br> Celebrity </br> Gossip",
-  "Who is singer Elle King's </br> famous father?", "Which of the following </br> fellas did not date </br> Taylor Swift?", "Who is Gwen Stefani's </br> current beau?", "What diet plan is Oprah </br> currently following?", "Who broke up with their </br> ex by ghosting them?", "GAME OVER"];
-  optionA = ["Press to Play","Kevin Bacon", "None of these options", "Titanic", "Kate Winslet", "Matrix", "Press to Play", "Alec Baldwin", "Adrien Brody", "Adam Sandler", "Steve Martin", "Darrell Hammond", "Press to Play", "Rob Schneider", "Dave Franco", "Blake Shelton", "Weight Watchers", "Charlize Theorn"];
-  optionB = ["", "Tom Cruise", "Django Unchained", "The Godfather", "Audrey Hepburn","Mrs. Doubtfire", "", "Steve Martin", "Kanye West", "Jimmy Falon", "John Belushi", "Kenan Thompson", "", "Adam Sandler", "Harry Styles", "Gavin Rossdale", "Nutri-System", "Taylor Swift", ""];
-  optionC = ["", "Samuel L. Jackson", "Titanic", "Citizen Kane", "Whoopi Goldberg", "Pitch Perfect", "", "John Goodman", "Miley Cryus", "Dan Aykroyd", "Chevy Chase", "Seth Meyers", "", "Joey King", "John Mayer", "Adam Levine", "The Atkins Diet Program", "John Mayer",""];
-  optionD = ["", "Morgan Freeman", "The Wolf of Wall Street", "Star Wars", "Tracy Jordan", "Die Hard", "", "Christopher Walken", "Paris Hilton", "Chevy Chase", "Dan Aykroyd", "Fred Armisen", "","Elvis Presley", "Jake Gyllenhaal", "Chris Martin", "Jenny Craig", "Katy Perry",""];
-  correctAnswer = ["Kevin Bacon", "None of these options", "Titanic", "Kate Winslet", "Matrix", "", "Alec Baldwin", "Adrien Brody", "Adam Sandler", "Steve Martin", "Darrell Hammond", "", "Rob Schneider", "Dave Franco", "Blake Shelton", "Weight Watchers", "Charlize Theorn"];
+  // round = ["ROUND 1 </br> TV & Movies", "ROUND 2 </br> Saturday Night </br> Live", "ROUND 3 </br> Celebrity </br> Gossip"];
+  question = ["Which actor has a game based </br> off connecting any celebrity</br> to him in 6 degrees?", "For which role did </br> Leonardo DiCaprio </br> win an Oscar?", "Which movie has </br> won the </br> most Oscars?", "Which of these </br> actors has not </br> received an EGOT?", "Which movie </br> is not </br> based off a book?", "Who has hosted </br> Saturday Night Live </br> the most times?", "Who has been </br> banned from <br> SNL?", "Who has not </br> been an anchor on </br> Weekend Update?", "Who was not </br> part of the <br> original SNL cast?", "Who was been a SNL </br> cast member the longest?", "Who is singer Elle King's </br> famous father?", "Which of the following </br> fellas did not date </br> Taylor Swift?", "Who is Gwen Stefani's </br> current beau?", "What diet plan is Oprah </br> currently following?", "Who broke up with their </br> ex by ghosting them?","Game Over"];
+  optionA = ["Kevin Bacon", "None of these options", "Titanic", "Kate Winslet", "Matrix", "Alec Baldwin", "Adrien Brody", "Adam Sandler", "Steve Martin", "Darrell Hammond", "Rob Schneider", "Dave Franco", "Blake Shelton", "Weight Watchers", "Charlize Theorn", "Thank You", "Thank You"];
+  optionB = ["Tom Cruise", "Django Unchained", "The Godfather", "Audrey Hepburn","Mrs. Doubtfire", "Steve Martin", "Kanye West", "Jimmy Falon", "John Belushi", "Kenan Thompson", "Adam Sandler", "Harry Styles", "Gavin Rossdale", "Nutri-System", "Taylor Swift", "for playing!", "for playing!"];
+  optionC = ["Samuel L. Jackson", "Titanic", "Citizen Kane", "Whoopi Goldberg", "Pitch Perfect", "John Goodman", "Miley Cryus", "Dan Aykroyd", "Chevy Chase", "Seth Meyers", "Joey King", "John Mayer", "Adam Levine", "The Atkins Diet Program", "John Mayer", "Want another chance?", "Want another chance?"];
+  optionD = ["Morgan Freeman", "The Wolf of Wall Street", "Star Wars", "Tracy Jordan", "Die Hard", "Christopher Walken", "Paris Hilton", "Chevy Chase", "Dan Aykroyd", "Fred Armisen", "Elvis Presley", "Jake Gyllenhaal", "Chris Martin", "Jenny Craig", "Katy Perry", "Press to play again", "Press to play again"];
+  correctAnswer = ["Kevin Bacon", "None of these options", "Titanic", "Kate Winslet", "Matrix", "Alec Baldwin", "Adrien Brody", "Adam Sandler", "Steve Martin", "Darrell Hammond", "Rob Schneider", "Dave Franco", "Blake Shelton", "Weight Watchers", "Charlize Theorn"];
 
   function questionAnswers(i){
     question1.html(question[i]);
@@ -131,20 +131,25 @@ function trivia(){
 
   function next (){
     //  console.log(this.classList.contains("correctAnswer"));
-    var index =parseInt( question1.attr("data-index"));// question1.dataset.index
+    var index =parseInt( question1.attr("data-index"));
+    // question1.dataset.index
     if (question.length == index) {
       alert("Game Over");
+      $('.container').hide();
     } else {
       questionAnswers(1 + index);
     }
   }
   questionAnswers(i)
 
+
   // calculate score
   // function showScore(){
   // 	$('.score').append("Your score is" count + " out of 5");
   // }
 }
+
 popout()
 trivia()
 // showScore()
+// gameOver()
